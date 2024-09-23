87feefb9e164d810591795f92856303d1280d675
JauniusPinelis <JauniusPinelis@gmail.com>
2024-09-23 18:07:58 +0300

    update

--- samples/fe-demo/cypress/e2e/spec.cy.js
+++ samples/fe-demo/cypress/e2e/spec.cy.js
@@ -1,48 +1,97 @@
 const pageUrl = "http://127.0.0.1:8080/"
 
-describe('Automated tests for local website works', () => {
-  it('the page loads successfully', () => {
+function successfullSubmit(name, email, phone){
+  cy.get("#name").type(name)
+  cy.get("#email").type(email)
+  cy.get("#phone").type(phone)
+  cy.get("#dob").type("1990-01-14")
+
+  cy.contains('button','Submit').click()
+}
+
+describe('Website loads elements correctly', () => {
+
+  beforeEach(() => {
     cy.visit(pageUrl)
   })
 
-  it('display Registration form on load', () => {
-    cy.visit(pageUrl)
-
-    cy.get("h2").should('have.text', 'Registration Form')
-    cy.contains('Registration Form')
-    cy.contains('h2','Registration Form')
+  describe("Form loading", () => {
+    it('display Registration form on load', () => {
+      cy.get("h2").should('have.text', 'Registration Form')
+      cy.contains('Registration Form')
+      cy.contains('h2','Registration Form')
+    })
+  
+    it('display submit button on load', () => {
+      cy.get("button").should('have.text', 'Submit')
+      cy.contains('button','Submit').click()
+    })
   })
 
-  it('display submit button on load', () => {
-    cy.visit(pageUrl)
-    cy.get("button").should('have.text', 'Submit')
-    cy.contains('button','Submit')
-  })
+  
 
-  it('Allows to fill the data', () => {
-    cy.visit(pageUrl)
+  it('displays the table', () => {
     cy.get("#name").type('Jaunius')
     cy.get("#email").type('JauniusPinelis@gmail.com')
 
+    // Action
     cy.get("button").click()
+
+    // Assert
   })
+
 })
 
-// Check that all elements load
+describe('Submit functionality', () => {
 
-  // Check that REgistration title is displayed
+  const name = "Jaunius";
+  const email = "JauniusPinelis@gmail.com"
+  const phone = "86941123"
 
-  // Check that form is visible and empty
+  beforeEach(() => {
+    cy.visit(pageUrl)
+  })
 
-  // Check that the Submit button is visible.
+  it('allows to fill data, submit and shows up in the table', () => {
+   
+    successfullSubmit(name, email, phone)
 
-  // The table is visible empty.
+    cy.get('#infoTable tbody tr').should('have.length', 1)
+    cy.get('#infoTable tbody tr')
+    .first()  // Select the first row
+    .find('td')  // Find all <td> elements in the first row
+    .then(($tds) => {
+      // Access individual cells
+      const nameValue = $tds.eq(0).text(); // Get text from the first column
+      const emailValue = $tds.eq(1).text(); // Get text from the second column
+      const phoneValue = $tds.eq(2).text(); // Get text from the third column
+  
+      expect(nameValue).to.equal(name);
+      expect(emailValue).to.equal(email);
+      expect(phoneValue).to.equal(phone);
+    });
+  })
 
-// Happy path functionlity
+  it('submits clears data', () => {
 
-  // I am able to fill data and submit it.
+    successfullSubmit(name, email, phone)
 
-    // The table contains the data i have sent
-    // After the sending the registration form gets emptied.
+    cy.get("#name").should("have.text", "")
+  })
 
-// Edges cases
+  it('display submit button on load', () => {
+    cy.get("button").should('have.text', 'Submit')
+    cy.contains('button','Submit').click()
+  })
+
+  it('Allows to fill the data', () => {
+    cy.get("#name").type('Jaunius')
+    cy.get("#email").type('JauniusPinelis@gmail.com')
+
+    // Action
+    cy.get("button").click()
+
+    // Assert
+  })
+
+})
